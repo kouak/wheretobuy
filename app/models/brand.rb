976d08e2b5bdd5d7f2403e1212205928ade20c4e
Benjamin Beret <kouak@macbook-de-benjamin-beret.local>
2010-02-09 14:21:14 +0100

    Formtastic for brands

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,8 @@
 class Brand < ActiveRecord::Base
   attr_accessible :name, :url
+  
+  validates_presence_of :name
+  validates_length_of :name, :in => 2..70
+  
+  validates_format_of :url, :with => /^(http):\/\/.*/, :allow_nil => true, :allow_blank => true
 end
